353d959fa5306cb5f17f86e4204b044938db0f6b
lluisfsegui <lluisfelipsegui@paucasesnovescifp.cat>
2026-01-14 17:47:07 +0100

    Refactor scriptftp.sh for improved readability

--- scriptftp.sh
+++ scriptftp.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Configuració
 USER_HOME="/home/Lluis"          # Directori home de l'usuari
 BACKUP_DIR="/tmp/backups"            # Directori temporal per guardar la còpia
 FTP_SERVER="192.168.144.198"         # Adreça del servidor FTP
@@ -8,8 +6,6 @@ FTP_USER="scriptFTP"                # Nom d'usuari del servidor FTP
 FTP_PASSWORD="12345678"       # Contrasenya del servidor FTP
 BACKUP_NAME="$FTP_USER-backup_$(date +%Y%m%d).tar.gz"  # Nom del fitxer de còpia amb la data actual
 FTP_TARGET_DIR="backups"            # Directori al servidor FTP on es pujarà la còpia
-
-# Funció per comprovar si hi ha errors
 function check_error {
     if [ $? -ne 0 ]; then
         echo "Error: $1"
@@ -17,7 +13,6 @@ function check_error {
     fi
     echo $?
 }
-
 # Crear el directori de còpies si no existeix
 mkdir -p "$BACKUP_DIR"
 echo "Directori creat correctament"
@@ -47,4 +42,5 @@ echo "Còpia de seguretat pujada correctament al servidor FTP."
 # rm "$BACKUP_DIR/$BACKUP_NAME"
 
 echo "Procés completat."
+
 
