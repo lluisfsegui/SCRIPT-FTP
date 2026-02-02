65a1480e839107b993c634d16f027f576df3b04f
lluisfsegui <lluisfelipsegui@paucasesnovescifp.cat>
2026-02-02 16:21:46 +0100

    Update FTP backup script with new user credentials

--- scriptftp.sh
+++ scriptftp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-USUARIO="engabiat1"
+USUARIO="usuario1"
 HOME_DIR="/home/$USUARIO"
 
 FECHA=$(date +"%Y-%m-%d_%H-%M")
 BACKUP_FILE="/tmp/${USUARIO}_backup_${FECHA}.tar.gz"
 
 FTP_SERVER="10.18.30.162"
-FTP_USER="usuario_backup"
+FTP_USER="engabiat"
 FTP_PASS="12345678"
 FTP_DIR="ftp_backups"
 
@@ -23,5 +23,6 @@ EOF
 rm "$BACKUP_FILE"
 
 echo "Backup completado correctamente."
+
 
 
